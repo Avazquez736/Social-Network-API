1e9b88b0f6aef108d8bf4e86c3cb05b83ddcc1fd
avazquez736 <alvarovazquez736@gmail.com>
2023-08-10 21:18:37 -0600

    created config and controllers

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+// Importing mongoose library
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONOGDB_URI || '',{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
+
+
+module.exports = mongoose.connection
